a86c55d07ac88b4fdeaaa869d708fd8651be181e
Palalde <Palessandrini13@gmail.com>
2026-02-19 00:02:31 +0100

    feat(useShifts): CRUD Add/update/delete/GetById

--- mon-app-react/src/features/shifts/hooks/useShifts.js
+++ mon-app-react/src/features/shifts/hooks/useShifts.js
@@ -1,9 +1,53 @@
+import { useLocalStorage } from "@/hooks";
 import { DEFAULT_SHIFTS } from "@/constants";
+import { timeToMinutes, generateId } from "@/utils";
 
 export default function useShifts() {
-  const shifts = DEFAULT_SHIFTS;
+  // states
+  const [shifts, setShifts] = useLocalStorage("shifts", DEFAULT_SHIFTS);
 
+  // add
+  const addShift = (shiftData) => {
+    // passer les heures (string) en minutes (nombres)
+    const startMn = timeToMinutes(shiftData.startTime);
+    const endMn = timeToMinutes(shiftData.endTime);
+
+    // calculer la diff et passer en heurs
+    const hours = (endMn - startMn) / 60;
+
+    // creer le shifts avec shiftData et hours
+    setShifts([...shifts, { ...shiftData, hours: hours, id: generateId() }]);
+  };
+
+  // update / edit
+  const updateShift = (shiftData) => {
+    // passer les heures (string) en minutes (nombres)
+    const startMn = timeToMinutes(shiftData.startTime);
+    const endMn = timeToMinutes(shiftData.endTime);
+
+    // calculer la diff et passer en heurs
+    const hours = (endMn - startMn) / 60;
+
+    // l'objet shiftData avec hours recalculer
+    const updatedShift = { ...shiftData, hours: hours };
+
+    // remplace le shift dans le state ou en créée un nouveau
+    setShifts(shifts.map((s) => (s.id === updatedShift.id ? updatedShift : s)));
+  };
+
+  // deleteShift
+  const deleteShift = (shiftId) => {
+    setShifts(shifts.filter((s) => s.id !== shiftId));
+  };
+
+  // getShiftById
   const getShiftById = (shiftId) => shifts.find((s) => s.id === shiftId);
 
-  return { shifts, getShiftById };
+  return {
+    shifts,
+    updateShift,
+    addShift,
+    deleteShift,
+    getShiftById,
+  };
 }
