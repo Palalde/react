f58248ff0fb9ab14876712c63f02c14ecc69ca0c
Palalde <Palessandrini13@gmail.com>
2026-03-17 18:01:36 +0100

    Refactor(useLocalReducer): Js to ts

--- src/hooks/useLocalReducer.ts
+++ src/hooks/useLocalReducer.ts
@@ -1,11 +1,17 @@
+import { Dispatch, Reducer } from "react";
 import useLocalStorage from "./useLocalStorage";
 
-export default function useLocalReducer(key, reducer, initialValue) {
+export default function useLocalReducer<T, A>(
+  key: string,
+  reducer: Reducer<T, A>,
+  initialValue: T,
+): [T, Dispatch<A>] {
   // useLocalStorage pour stocker l'état dans le localStorage
   const [state, setState] = useLocalStorage(key, initialValue);
 
   //  dispatch qui applique le reducer et met à jour le localStorage
-  const dispatch = (action) => setState((prev) => reducer(prev, action));
+  const dispatch: Dispatch<A> = (action) =>
+    setState((prev) => reducer(prev, action));
 
   return [state, dispatch];
 }
