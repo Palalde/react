7a14385302f081eecccff4595b811c9fc0072b7b
Palalde <Palessandrini13@gmail.com>
2026-07-29 22:35:40 +0200

    refacto(theme): js to ts

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,8 +1,22 @@
 import { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
-export default function useTheme() {
-  const [theme, setTheme] = useLocalStorage("theme", "system");
+type Theme = "light" | "dark" | "system";
+
+/**
+ * Custom hook to manage theme (light, dark, system)
+ * @returns { theme, setTheme, toggleTheme }
+ * - theme: current theme
+ * - setTheme: function to set the theme
+ * - toggleTheme: function to toggle between light and dark mode
+ */
+
+export default function useTheme(): {
+  theme: Theme;
+  setTheme: React.Dispatch<React.SetStateAction<Theme>>;
+  toggleTheme: () => void;
+} {
+  const [theme, setTheme] = useLocalStorage<Theme>("theme", "system");
 
   useEffect(() => {
     // Appliquer le thème en modifiant la classe du root element
